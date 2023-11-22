96154eb447a57490f70bee0561f772eabd65554d
AudeTercero <ikerdam1b@gmail.com>
2023-11-22 23:23:35 +0100

    Creacion de proyecto

--- com/dam/GestionVehiculos.py
+++ com/dam/GestionVehiculos.py
@@ -1,40 +1,43 @@
+def alta():
+    print()
 
 
+def baja():
+    print()
 
-class GestionVehiculos:
-    def alta(self):
-        print()
 
-    def baja(self):
-        print()
+def modificar():
+    print()
 
-    def modificar(self):
-        print()
 
-    def buscar(self):
-        print()
+def buscar():
+    print()
 
-    def mostrar(self):
-        print()
-    def menu(self):
-        salir = True
-        while (salir):
-            opc = input(
-                "\t****GESTION VEHICULOS****\n 1. Alta\n 2. Baja\n 3. Modificar\n 4. Buscar \n 5. Mostrar todos\n 0. Salir\n ")
-            if opc == "1":
-                self.alta()
-            elif opc == "2":
-                self.baja()
-            elif opc == "3":
-                self.modificar()
-            elif opc == "4":
-                self.buscar()
-            elif opc == "5":
-                self.mostrar()
-            elif opc == "0":
-                print("Saliendo...")
-                salir = False
-            else:
-                print("Esa opcion no existe")
+
+def mostrar():
+    print()
+
+
+def menu():
+    salir = True
+    while (salir):
+        opc = input(
+            "\t****GESTION VEHICULOS****\n 1. Alta\n 2. Baja\n 3. Modificar\n 4. Buscar \n 5. Mostrar todos\n 0. Salir\n ")
+        if opc == "1":
+            alta()
+        elif opc == "2":
+            baja()
+        elif opc == "3":
+            modificar()
+        elif opc == "4":
+            buscar()
+        elif opc == "5":
+            mostrar()
+        elif opc == "0":
+            print("Saliendo...")
+            salir = False
+        else:
+            print("Esa opcion no existe")
+
 
 
