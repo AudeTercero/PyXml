fdb0e07015b9d88c9722263796611b91d5865303
AudeTercero <ikerdam1b@gmail.com>
2023-11-22 23:24:07 +0100

    Creacion de proyecto

--- com/dam/Menu.py
+++ com/dam/Menu.py
@@ -2,7 +2,7 @@ import GestionVehiculos
 import GestionAlquiler
 print("Iniciamos Gestion de Alquileres")
 salir = True
-gesVehi = GestionVehiculos.GestionVehiculos()
+gesVehi = GestionVehiculos
 gesAlq = GestionAlquiler
 while(salir):
     opc = input("\t\t****MENU****\n 1. Gestion de Vehiculos\n 2. Gestion de Alquiler\n 0. Salir\n")
